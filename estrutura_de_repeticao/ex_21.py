da650132b1e3cd296d0f80da6b8b9003d63da7a4
João Vitor Penha de Souza <100869890+mrPenha@users.noreply.github.com>
2022-09-09 14:47:05 -0300

    python excercises

--- estrutura_de_repeticao/ex_21.py
+++ estrutura_de_repeticao/ex_21.py
@@ -2,101 +2,20 @@
 import os
 os.system('clear')
 
+num = int(input('Insira um número: '))
+total = 0
 
-#  ainda tentando resolver isso
+for i in range(1, num + 1):
+    if num % i == 0:
+        total += 1
 
+    else:
+        pass
 
-# cont = 1
-# while cont > 0:
-#     n = input('Insira um número inteiro: ')
+if total == 2:
+    print(f'\nEste número é primo\n')
 
-#     if ',' or '.' not in n:
-#         n = int(n)
+else:
+    print(f'\nEste número não é primo\n')
 
-#         if n != 0 and n != 1:
-
-#             if n % 2 != 0 and n is not 2:
-
-#                 x = 0  # número de vezes que é divisível
-#                 c = 1  # divisor
-#                 q = 0  # quociente
-#                 r = 0  # resto da divisão
-#                 while c > 0:
-
-#                     #  quociente
-#                     q = n/c
-
-#                     r = n % c
-
-#                     if c == 1 and n/c == n and q is not float:
-#                         print('\nEste número é primo\n')
-#                         x += 1
-#                         c + 1
-
-#                     else:
-#                         print('\nEste número não é primo\n')
-
-#                     if c == n and n/c == 1 and q is not float:
-#                         print('\nEste número é primo\n')
-#                         x += 1
-#                         c += 1
-
-#                     else:
-#                         print('\nEste número não é primo\n')
-
-#                     if x >= 2:
-#                         print('\nEste número não é primo\n')
-#                         break
-
-#                     else:
-#                         pass
-
-#             else:
-#                 print('\nNúmeros pares não são primos, exceto o número 2\n')
-#                 tentar = input('Gostaria de tentar novamente? [S][N]: ')
-#                 tentar = tentar.lower()
-
-#                 if tentar == 's':
-#                     os.system('clear')
-#                     print()
-#                     cont = 1
-#                 else:
-#                     print()
-#                     quit()
-
-#         else:
-#             print('\n0 e 1 não são números primos\n')
-#             tentar = input('Gostaria de tentar novamente? [S][N]: ')
-#             tentar = tentar.lower()
-
-#             if tentar == 's':
-#                 os.system('clear')
-#                 print()
-#                 cont = 1
-#             else:
-#                 print()
-#                 quit()
-
-#     else:
-#         print('\nInsira apenas números inteiros\n')
-#         tentar = input('Gostaria de tentar novamente? [S][N]: ')
-#         tentar = tentar.lower()
-
-#         if tentar == 's':
-#             os.system('clear')
-#             print()
-#             cont = 1
-#         else:
-#             print()
-#             quit()
-
-#     sair = input('Gostaria de continuar? [S][N]: ')
-#     sair = sair.lower()
-
-#     if sair == 's':
-#         os.system('clear')
-#         print()
-#         cont += 1
-#     else:
-#         print('\nVolte Sempre! \n')
-#         quit()
+# print(f'\nO número foi divisível {total} vezes\n')
