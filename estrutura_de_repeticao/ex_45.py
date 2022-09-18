2d739a29326d6bc6bc545f5b2d11137cb3b71984
João Vitor Penha de Souza <100869890+mrPenha@users.noreply.github.com>
2022-09-18 12:36:41 -0300

    python excercises

--- estrutura_de_repeticao/ex_45.py
+++ estrutura_de_repeticao/ex_45.py
@@ -20,59 +20,95 @@
     Após concluir isto você poderia incrementar o programa permitindo que o professor digite o gabarito da prova antes dos alunos usarem o programa."""
 import os
 os.system('clear')
+qtd_acerto = []
+qtd_erro = []
+nota_maxima = 10
+nota = []
+acerto = 0
+erro = 0
+gabarito = ''
+resposta = ''
+nome_do_aluno = []
+qtd_alunos = 0
+op = -1
+while op != 4:
+    print('Escolha a opção \n1 - Professor \n2 - Aluno \n3 - Resultados \n4 - finalizar')
+    print('----------------')
 
-while True:
-
-    print('VOCÊ É... \n(digite o número correspondente) \n\n1 - Professor \n2 - Aluno')
-    print('----------')
     op = int(input('> '))
 
-    if op == 1:
-        os.system('clear')
-        print('\nInsira as respostas como A, B, C, D, e E\n')
-        qtd = int(input('Quantidade de questões: '))
-
-        resp = []
-        for i in range(1, qtd+1):
-            r = input(f'Questão {i}: ').upper()
-            resp += r
-
-        gabarito = []
-        for gabarito in resp:
-            gabarito = resp
-
-        os.system('clear')
-
-    elif op == 2:
-        os.system('clear')
-        print('\nInsira as respostas como A, B, C, D, e E\n')
-        print('\nGABARITO\n')
-
-        resposta_aluno = []
-        for i in range(1, qtd+1):
-            r = input('Resposta: ').upper()
-            resposta_aluno += r
+    if op == 4:
         break
 
-    else:
+    #  entrada do professor (qtde questões e gabarito de respostas)
+    elif op == 1:
         os.system('clear')
-        print('Opção Inválida\n\n')
+        qtd_questoes = int(input('Quantidade de Questões: '))
+        print('Insira as respostas como sendo A, B, C, D ou E \n')
 
-print()
-print('\nRESPOSTAS\n')
-print(f'\nGabarito : {gabarito}')
-print(f'\nRespostas: {resposta_aluno}')
+        c = 0
+        while c < qtd_questoes:
+            r = input(f'Resposta da questão {c+1}: ')
+            if r == 'a' or r == 'b' or r == 'c' or r == 'd' or r == 'e':
+                gabarito += r.upper()
+                c += 1
+            else:
+                print('\nInsira as respostas como sendo A, B, C, D ou E \n')
+                c = c
 
-certo_errado = []
-c = 0
-while c < qtd:
+        os.system('clear')
 
-    if gabarito[c] == resposta_aluno[c]:
-        certo_errado += 'v'
-    else:
-        certo_errado += 'x'
+    #  entrada do aluno (nome e respostas)
+    elif op == 2:
+        os.system('clear')
+        nome = input('Nome do Aluno: ')
+        print('Insira as respostas como sendo A, B, C, D ou E \n')
+        nome_do_aluno.append(nome)
+        qtd_alunos += 1
 
-    c += 1
+        c = 0
+        while c < qtd_questoes:
+            r = input(f'Resposta da questão {c+1}: ')
+            if r == 'a' or r == 'b' or r == 'c' or r == 'd' or r == 'e':
+                resposta += r.upper()
+                c += 1
+            else:
+                print('\nInsira as respostas como sendo A, B, C, D ou E \n')
+                c = c
 
-print()
-print(f'Acertos  : {certo_errado}')
+        for i in range(qtd_questoes):
+            if resposta[i] == gabarito[i]:
+                acerto += 1
+            else:
+                erro += 1
+
+        qtd_acerto.append(acerto)
+        qtd_erro.append(erro)
+        nota.append((nota_maxima/qtd_questoes)*acerto)
+
+        os.system('clear')
+
+    #  resultados (gabarito x resposta(por aluno))
+    elif op == 3:
+        os.system('clear')
+        x = 0
+        while x < qtd_alunos:
+            print(f'Aluno: {nome_do_aluno[x]} \n')
+            print(f'Gabarito --- Respostas')
+            print('-----------------------')
+            x += 1
+
+            c = 0
+            while c < qtd_questoes:
+                print(f'{gabarito[c]}            {resposta[c]}')
+                print('-----------------------')
+                c += 1
+
+            print()
+            c += 1
+
+        input('\nPressione ENTER para sair dos resultados \n')
+        os.system('clear')
+
+
+os.system('clear')
