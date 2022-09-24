70879fa49170a82c5d7a2536f60ddcb94d13020f
João Vitor Penha de Souza <100869890+mrPenha@users.noreply.github.com>
2022-09-23 23:26:24 -0300

    aulas de python

--- aulas/testes.py
+++ aulas/testes.py
@@ -1,11 +1,18 @@
 import os
 os.system('clear')
 
-lista = [
-    ['joao', 'maria', 'ricardo'],
-    ['monique', 'dorith', 'franco'],
-    ['frango', 'macarrao', 'gato'],
-]
+"""
+0 10
+1  9
+2  8
+3  7
+4  6
+5  5
+6  4
+7  3
+8  2
+9  1
+"""
 
-enumerada = enumerate(lista)
-print(list(enumerada), '\n')
+for i, j in enumerate(range(10, 1, -1)):
+    print(i, '-', j)
